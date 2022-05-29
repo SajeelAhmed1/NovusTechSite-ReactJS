0f71851c5969c3499b625470b6391147b233fe0a
SajeelAhmed1 <sajeelahmedsiddiqui@gmail.com>
2022-05-30 03:01:51 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Spline from '@splinetool/react-spline';
+ 
+function App() {
+  
+  
+  return (
+    <div className="flex w-screen h-screen min-h-screen flex-col items-center justif-center relative bg-primary pb-20 ">
+      <div > 
+      {/* <Spline scene="https://prod.spline.design/RPndWCj2FgpxBZIl/scene.splinecode" /> */}
+      <Spline scene="https://prod.spline.design/RPndWCj2FgpxBZIl/scene.splinecode" />
+  
+  
+  
+      </div>
+    </div>
+  );
+}
+
+export default App;
