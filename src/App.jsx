a5ecd8ec6c178794a0b71bf33b506cbf782f7177
SajeelAhmed1 <sajeelahmedsiddiqui@gmail.com>
2022-05-30 15:12:09 +0500

    new model added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import Spline from '@splinetool/react-spline';
+import {IoMenu} from 'react-icons/io5'
+import { useState } from 'react';
  
 function App() {
+
+  const [isActive, setIsActive] = useState(false);
   
   
   return (
@@ -13,11 +17,68 @@ function App() {
               <a href="#home"
                 className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
               > Home</a>
+              <a href="#home"
+                className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+              > About</a>
+              <a href="#home"
+                className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+              > Contact</a>
           </div>
+          <div
+              className="block md:hidden  ml-auto cursor-pointer"
+              onClick={() => setIsActive(!isActive)}
+            >
+              <IoMenu className="text-2xl text-textBase " />
+            </div>
+            {isActive && (
+              <div
+                
+                className="p-4 w-275 bg-navBar rounded-lg fixed top-24 right-16 flex flex-col items-center justify-evenly gap-6"
+              >
+                <a
+                  href="#home"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+                  onClick={() => setIsActive(false)}
+                >
+                  Home
+                </a>
+                <a
+                  href="#about"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+                  onClick={() => setIsActive(false)}
+                >
+                  About
+                </a>
+                <a
+                  href="#projects"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+                  onClick={() => setIsActive(false)}
+                >
+                  Projects
+                </a>
+                <a
+                  href="#contacts"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+                  onClick={() => setIsActive(false)}
+                >
+                  Contact
+                </a>
+                <a
+                  href="#"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in"
+                  onClick={() => setIsActive(false)}
+                >
+                  Download
+                </a>
+              </div>)}
         </div>
       </nav>
-      <div className="relative" id="home" > 
-      <Spline scene="https://prod.spline.design/RPndWCj2FgpxBZIl/scene.splinecode" />
+      <div className="relative " id="home" > 
+      
+      {/* new bg */}
+      <Spline scene="https://prod.spline.design/BdDckWFpugA51Fok/scene.splinecode" />
+
+ 
       </div>
     </div>
   );
