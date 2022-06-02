8bbb8388f11ee98811e58f18de653053705edc59
SajeelAhmed1 <sajeelahmedsiddiqui@gmail.com>
2022-06-02 12:46:08 +0500

    new cmmit

--- src/App.jsx
+++ src/App.jsx
@@ -96,63 +96,56 @@ function App() {
             </div>
             <div className="w-full h-420 flex flex-col items-center justify-center ">
               <p className="text-lg text-textBase text-center">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Sapiente dicta recusandae commodi praesentium officia, aliquam
-                cumque voluptatem dolor enim ducimus id maiores consequatur
-                dolorem! Architecto aperiam corrupti nostrum eum similique!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Veritatis saepe dolor illo. Tenetur labore impedit nihil natus
-                explicabo perspiciatis nisi autem aut numquam dolorum, voluptas
-                vero nobis assumenda commodi magni.
+                Providng the best ad most effeciently streamlined solution for all your IT needs and beyond.
               </p>
 
               <button class="w-full md:w-auto relative mt-6 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-orange-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800 hover:shadow-lg hover:shadow-teal-500/50 hover:dark:shadow-lg hover:dark:shadow-teal-800/80">
                 <span class="w-full md:w-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                  Download
+                  Hire Us !
                 </span>
               </button>
             </div>
             </section>
         </main>
-        <h1 className='text-m text-6xl'>Our Services</h1>
+        <h1 className='u text-6xl'>Our Services</h1>
         <section
             className="flex flex-wrap items-center justify-evenly my-24 gap-4"
             id="projects"
           >
-        <div class="service">
-          <i class="fas fa-laptop-code"></i>
-          <h2>Web Design</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-            quae?
-          </p>
-        </div>
+        <div className='cont'>
+                <div class="service">
+                <i class="fas fa-laptop-code"></i>
+                <h2>Web Design</h2>
+                <p className='ptext'>
+                    Web Solutions ranging from Front End developement to backend developement solutions
+                </p>
+                </div>
 
-        <div class="service">
-          <i class="fas fa-laptop-code"></i>
-          <h2>Web Design</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-            quae?
-          </p>
-        </div>
+                <div class="service">
+                <i class="fas fa-mobile-alt"></i>
+                <h2>App Development</h2>
+                <p className='ptext'>
+                    Robust Mobile Application developement using a wide array of trending frameworks and Languages 
+                </p>
+                </div>
 
-        <div class="service">
-          <i class="fas fa-laptop-code"></i>
-          <h2>Web Design</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-            quae?
-          </p>
-        </div>
-        <div class="service">
-          <i class="fas fa-laptop-code"></i>
-          <h2>Web Design</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-            quae?
-          </p>
-        </div>
+                <div class="service">
+                <i class="fas fa-chart-line"></i>
+                <h2>Marketing</h2>
+                <p className='ptext'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+                    quae?
+                </p>
+                </div>
+                <div class="service">
+                <i class="fab fa-sketch"></i>
+                <h2>Graphics</h2>
+                <p className='ptext'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+                    quae?
+                </p>
+                </div>
+            </div>
 
                 
                 
